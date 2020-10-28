6b2579f9e8cb201dfeab97996c7c5832ed0d9bb6
ASHUTOSH <ashutosh@192.168.1.6>
2020-10-29 00:56:40 +0530

    [Ashutosh]Added:Maximum 4s and 6s

--- src/main/java/com/ipl/Runs.java
+++ src/main/java/com/ipl/Runs.java
@@ -3,33 +3,46 @@ package com.ipl;
 import com.opencsv.bean.CsvBindByName;
 
 public class Runs {
-	@CsvBindByName
-	public int Pos;
-	@CsvBindByName
+	@CsvBindByName(column = "POS")
+	public int pos;
+
+	@CsvBindByName(column = "PLAYER")
 	public String Player;
-	@CsvBindByName
-	public int Mat;
-	@CsvBindByName
-	public int Inns;
-	@CsvBindByName
-	public int NO;
-	@CsvBindByName
-	public int Runs;
-	@CsvBindByName
-	public String HS;
-	@CsvBindByName
-	private String Avg;
-	@CsvBindByName
-	public int BF;
-	@CsvBindByName
+
+	@CsvBindByName(column = "Mat")
+	public int mat;
+
+	@CsvBindByName(column = "Inns")
+	public int inns;
+
+	@CsvBindByName(column = "NO")
+	public int no;
+
+	@CsvBindByName(column = "Runs")
+	public int runs;
+
+	@CsvBindByName(column = "HS")
+	public String hs;
+
+	@CsvBindByName(column = "Avg")
+	public String Avg;
+
+	@CsvBindByName(column = "BF")
+	public int bf;
+
+	@CsvBindByName(column = "SR")
 	public double SR;
-	@CsvBindByName
-	public int hundreds;
-	@CsvBindByName
-	public int fifties;
-	@CsvBindByName
+
+	@CsvBindByName(column = "100")
+	int hundreds;
+
+	@CsvBindByName(column = "50")
+	public int fiftys;
+
+	@CsvBindByName(column = "4s")
 	public int fours;
-	@CsvBindByName
+
+	@CsvBindByName(column = "6s")
 	public int sixes;
 
 	public double getAvg() {
